18a7cf656f0fca7e3a5b701b123829e9596ad2dc
Cristóbal Loyola <cristobal.loyola.m@gmail.com>
2023-01-27 13:14:36 -0300

    Minor changes

--- mmfewshot/detection/models/utils/transformers.py
+++ mmfewshot/detection/models/utils/transformers.py
@@ -245,13 +245,13 @@ class CrossAttentionTransformerBlock(nn.Module):
         self.query_compression = nn.Sequential(
             nn.Conv2d(self.in_channels, self.in_channels, 3, padding='same'),
             nn.BatchNorm2d(self.in_channels),
-            nn.ReLu(),
+            nn.ReLU(),
             nn.Conv2d(self.in_channels, self.embed_size, 1, padding='same'),
         )
         self.support_compression = nn.Sequential(
             nn.Conv2d(self.in_channels, self.in_channels, 3, padding='same'),
             nn.BatchNorm2d(self.in_channels),
-            nn.ReLu(),
+            nn.ReLU(),
             nn.Conv2d(self.in_channels, self.embed_size, 1, padding='same'),
         )
         self.dropout = nn.Dropout(dropout_prob)
